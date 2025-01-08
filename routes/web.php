15f7cd26198b83c6c895c39d3070dd6a27886da9
BrandonSegura <brandoncconejo@gmail.com>
2025-01-08 13:21:49 -0600

    Se inicia el proyecto

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/posts',[PostController::class, 'index']);
+Route::get('/posts/create',[PostController::class, 'create']);
+Route::get('/posts/{post}',[PostController::class, 'show']);
